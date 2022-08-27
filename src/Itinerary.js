37de6a39de4bd8b8cfaeb2289ac7958f1ae2e0cb
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-08-27 15:06:36 +0100

    itinerary

---
+++ src/Itinerary.js
@@ -0,0 +1,8 @@
+const Port = require('./Port');
+
+
+function Itinerary(ports) {
+this.ports = ports
+};
+
+module.exports = Itinerary;
---
+++ src/Itinerary.test.js
@@ -0,0 +1,17 @@
+const Itinerary = require('./Itinerary');
+const Port = require('./Port');
+
+
+describe('Itinerary', () => {
+    it('can be instantiated', () => {
+        expect(new Itinerary()).toBeInstanceOf(Object);
+    })
+    it('can have ports', () => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+       
+        const itinerary = new Itinerary([dover, calais])
+
+        expect(itinerary.ports).toEqual([dover, calais]);
+    })
+})
