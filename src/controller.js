e95a73ba4eaa35064a6f46294dfa1c45a503b87d
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-09-07 22:03:51 +0100

    sail

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,62 @@
 (function exportController() {
-function Controller () {
-  this.initialiseSea()
+function Controller(ship) {
+  this.ship = ship;
+  this.initialiseSea();
+  document.querySelector('#sailbutton').addEventListener('click', () => {
+    this.setSail();
+  });
 }
+Controller.prototype = {
+    renderPorts (ports) {
+        const portsElement = document.querySelector('#ports');
+portsElement.style.width = '0px';
+ports.forEach((port, index) => {
+    const newPortElement = document.createElement('div');
+    newPortElement.className = 'port';
+    newPortElement.dataset.portName = port.name;
+    newPortElement.dataset.portIndex = index;
+    const portsElementWidth = parseInt(portsElement.style.width, 10);
+portsElement.style.width = `${portsElementWidth + 256}px`;
+    portsElement.appendChild(newPortElement);
+  })
+    },
+  
+    renderShip() {
+      const ship = this.ship
+    
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+    
+      const shipElement = document.querySelector('#ship');
+      shipElement.style.top = `${portElement.offsetTop + 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+  },
+  setSail() {
+    const ship = this.ship
+
+const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+const nextPortIndex = currentPortIndex + 1;
+const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+if (!nextPortElement) {
+return alert('End of the line!');
+}
+const shipElement = document.querySelector('#ship');
+const sailInterval = setInterval(() => {
+  const shipLeft = parseInt(shipElement.style.left, 10);
+  if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+    ship.setSail();
+    ship.dock();
+    clearInterval(sailInterval);
+  }
+
+  shipElement.style.left = `${shipLeft + 1}px`;
+}, 20);
+
+
+  },
+
+  };
+
 Controller.prototype.initialiseSea = function initialiseSea () {
   const backgrounds = [
     './images/images/water0.png',
